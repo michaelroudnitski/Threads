194cd167e03dd953dd8bb9b1ba82d1e7068c4195
Lequanghy <Lequanghy1998@yahoo.ca>
2017-06-05 15:39:36 -0400

    Add files via upload

--- website/account/views.py
+++ website/account/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from catalog.models import Sex, Category, Product
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth import logout
 
 mcata = Sex.objects.get(sex_selection='m').category_set.all()
 wcata = Sex.objects.get(sex_selection='w').category_set.all()
@@ -11,7 +13,7 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/account')
+            return redirect('/accounts')
 
         else:
             form = UserCreationForm()
@@ -24,6 +26,27 @@ def register(request):
         context.update(cat_context)
         return render(request, 'account/registration.html', context)
 
+def profile(request):
+    context = {'user': request.user}
+    context.update(cat_context)
+    return render(request, 'account/Profile.html', context)
+
+def edit_profile(request):
+    if request.method == "POST":
+        form = UserChangeForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/accounts/profile')
+    else:
+        form = UserChangeForm(instance=request.user)
+        context={'form': form}
+        context.update(cat_context)
+        return render(request,'account/edit_profile.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('/catalog')
 
 def index(request):
     context = cat_context
